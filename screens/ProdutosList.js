df35b8425d3ae1a2296f0ef5e852501f54078e6e
texjuliana <julianateixeira150@gmail.com>
2022-01-09 13:26:43 -0300

    pagina inicial arrumada

--- screens/ProdutosList.js
+++ screens/ProdutosList.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import { View, Text,   FlatList, StyleSheet } from 'react-native';
+import { View, Text,   FlatList, StyleSheet , SafeAreaView, StatusBar} from 'react-native';
 
 import { Product } from '../components/Product.js';
 import { getProdutos } from '../servicos/livros.js';
 
-export function ProdutosList ({navigation}) {
+export function ProdutosList ({navigation, route}) {
 
   function renderProduct({item: product}) {
     return (
@@ -26,7 +26,10 @@ export function ProdutosList ({navigation}) {
   });
   
   return (
-    
+   <SafeAreaView style={styles.container}>
+         <StatusBar backgroundColor='#171d31' barStyle="ligth-content" /> 
+    <Text style={styles.title}>Loja da Ju</Text>
+    <Text style={styles.title}>Bem vindo(a) {route.params.input}</Text>
     <FlatList
       style={styles.ProdutosList}
       contentContainerStyle={styles.ProdutosListContainer}
@@ -34,6 +37,9 @@ export function ProdutosList ({navigation}) {
       data={Produtos}
       renderItem={renderProduct}
     />
+    </SafeAreaView>
+  
+ 
   );
 }
 
@@ -46,4 +52,48 @@ const styles = StyleSheet.create({
     paddingVertical: 8,
     marginHorizontal: 8,
   },
+  into:{
+    height: 480,
+    width: 320,
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+    borderColor: '#000',
+    alignSelf: 'center',
+    borderRadius: 7,
+    padding: 2,
+    textAlign: 'center',
+
+  },
+  input:{
+    fontSize:15,
+    marginLeft:10,
+    marginRight:10,
+    marginTop:30,
+    backgroundColor:'#ddd',
+    padding:9,
+    height: 45,
+    textAlignVertical: 'top',
+    color: '#000',
+    borderRadius: 5,
+  },
+  container:{
+    flex:1,
+    backgroundColor: "#DB7093"
+  },
+  title:{
+    marginTop: 10,
+    paddingBottom: 10,
+    fontSize: 25,
+    textAlign: 'center',
+    color: '#FFF'
+  },
+  subtitle:{
+    marginTop:10,
+    paddingBottom: 10,
+    fontSize: 15,
+    textAlign: 'center',
+    color: '#FFF',
+  }
 });
---
+++ src/Home/Home.js
@@ -0,0 +1,120 @@
+import React ,{ useState } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, StatusBar, 
+  TouchableOpacity, Modal, TextInput, Image, Button, FlatList } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import * as Animatable from 'react-native-animatable';
+const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity);
+
+const Stack = createNativeStackNavigator();
+
+
+export default function Home({ navigation }) {
+    const [input, setinput] = useState('') ;
+
+    const [door, setDoor] = useState(false);
+    const [descript, setDescript] = useState('');
+    
+    
+    
+  function verify(){
+    if (input === '') {alert('Espaço em branco, inserir nome!!'); 
+  }else {
+        return navigation.navigate("Produto", {nomeCli: input })
+    }
+
+    setDoor(true);
+  }
+      
+
+    return(
+        <SafeAreaView style={styles.container}>
+        <StatusBar backgroundColor='#FFB6C1' barStyle='light-content' />
+  
+       
+          <Text style={styles.title}>Loja de livros da Juliana Teixeira</Text>
+          
+          <TextInput 
+          placeholder='Nome do cliente'
+          placeholderTextColor='#747474'
+          value={input}
+          style={styles.input}
+          onChangeText={ (texto) => setInput(texto) }
+          />
+  
+            <TouchableOpacity onPress={verify} style={styles.head}>
+              <Ionicons style={{marginLeft: 5, marginRight: 5}}name="ios-add" size={35} color="#fff" />
+              <Text style={{color: "#fff"}}>Entrar</Text>
+            </TouchableOpacity>
+  
+        
+  
+  </SafeAreaView>
+
+    );
+
+}
+const styles = StyleSheet.create({
+  headerTitle: {
+    fontSize: 20
+  },
+  into:{
+    height: 480,
+    width: 320,
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+    borderColor: '#000',
+    alignSelf: 'center',
+    borderRadius: 7,
+    padding: 2,
+    textAlign: 'center',
+
+  },
+  input:{
+    fontSize:15,
+    marginLeft:10,
+    marginRight:10,
+    marginTop:30,
+    backgroundColor:'#ddd',
+    padding:9,
+    height: 45,
+    textAlignVertical: 'top',
+    color: '#000',
+    borderRadius: 5,
+  },
+  container:{
+    flex:1,
+    backgroundColor: "#DB7093"
+  },
+  title:{
+    marginTop: 10,
+    paddingBottom: 10,
+    fontSize: 25,
+    textAlign: 'center',
+    color: '#FFF'
+  },
+  subtitle:{
+    marginTop:10,
+    paddingBottom: 10,
+    fontSize: 15,
+    textAlign: 'center',
+    color: '#FFF',
+  },
+  enter:{
+    color: '#FFF',
+    fontSize: 20,
+    marginLeft: 15,
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  head:{
+    marginLeft:10,
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    color: '#000',
+    alignSelf: 'center',
+  }
+});
+
